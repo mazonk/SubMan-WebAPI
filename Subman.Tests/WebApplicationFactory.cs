96e1f625716f9dcaf57a8cdaf6ced65db73a5adb
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-04-09 09:16:29 +0200

    test

--- Subman.Tests/WebApplicationFactory.cs
+++ Subman.Tests/WebApplicationFactory.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc.Testing;
+dusing Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Subman.Database;
